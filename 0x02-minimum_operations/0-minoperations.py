5e6d4d2996b3c548109c23c6feea3a0244371a7c
SkywalkerDarth <danteuche@gmail.com>
2023-07-12 08:28:02 +0100

    new

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+""" A minimum_operations Interview """
+
+
+def minOperations(n)->int:
+    """ a function called minOperations
+    Args:
+        int n: given number
+
+    Return:
+        Integer, if successful, 0 if not
+    """
+    if n == 1:
+        return 0
+
+    operations = [0] * (n + 1)
+
+    for i in range(2, n + 1):
+        operations[i] = float('inf')
+        for j in range(1, i):
+            if i % j == 0:
+                operations[i] = min(operations[i], operations[j] + (i // j))
+
+    if operations[n] == float('inf'):
+        return 0
+    else:
+        return operations[n]
