1c7cd91c6680de0275c1d80e552c51a714370766
SkywalkerDarth <danteuche@gmail.com>
2023-07-27 18:01:09 +0100

    utf-8 coding

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+"""
+A method that determines if a given data set represents
+a valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    """
+    validUTF8 method
+    Args: data(int)
+    Return: False, True.
+    """
+    for byte in data:
+        if byte >> 7 == 0:
+            continue
+        elif byte >> 5 == 0b110:
+            continue
+        elif byte >> 4 == 0b1110:
+            continue
+        elif byte >> 3 == 0b11110:
+            continue
+        else:
+            return False
+    return True
