07bde8fe2e9687e1e893ad8eccbbc23df0b4cac8
SkywalkerDarth <danteuche@gmail.com>
2023-08-05 09:29:08 +0100

    newer

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,65 +1,48 @@
 #!/usr/bin/python3
 
 
-""" The N queens puzzle """
+""" N queens """
 
 
 import sys
 
 
-def is_safe(board, row, col):
-    """ is_safe
-    Args: board, row, col
-    Return
-        True
-    """
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-        if row - i - 1 >= 0 and board[row - i - 1][col - i - 1] == 1:
-            return False
-        if row + i + 1 < len(board) and board[row + i + 1][col - i - 1] == 1:
-            return False
-    return True
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-def solve_nqueens(board, col, n):
-    """ solve n_queens
-    Args: board, col, n
-    Return: bool
-    """
-    if col == n:
-        print_solution(board)
-        return
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
 
-    for i in range(n):
-        if is_safe(board, i, col):
-            board[i][col] = 1
-            solve_nqueens(board, col + 1, n)
-            board[i][col] = 0
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
 
-def print_solution(board):
-    """ prints out solution """
-    for row in board:
-        print(' '.join(str(cell) for cell in row))
+n = int(sys.argv[1])
 
-def main():
-    """main file"""
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
 
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
 
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
 
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    solve_nqueens(board, 0, n)
+def solver(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
 
-if __name__ == "__main__":
-    main()
+
+solve(n)
